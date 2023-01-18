d36c412549b65ca40b9d287ace733b6fa2fa6b3c
aprilyanggarwood <yangyyj822@hotmail.com>
2023-01-17 18:52:41 -0800

    documented openapi for findAllTasks and createTask

--- server/routes/employee-api.js
+++ server/routes/employee-api.js
@@ -15,7 +15,7 @@ const { update } = require("../models/employee");
 
 const router = express.Router();
 
-// find an employee by ID , findEmployeeById
+// Find an employee by ID , findEmployeeById
 /**
  * @openapi
  * /api/employees/{empId}:
@@ -66,12 +66,39 @@ router.get("/:empId", async (req, res) => {
   }
 });
 
+// Find all tasks by empId, findAllTasks
 /**
- * findAllTasks
+ * @openapi
+ * /api/employees/{empId}/tasks:
+ *   get:
+ *     tags:
+ *       - Employees
+ *     name: findAllTasks
+ *     description: API to show all tasks by empId
+ *     summary: Find all tasks by empId
+ *     parameters:
+ *       - name: empId
+ *         in: path
+ *         required: true
+ *         description: employee's id is created in MongoDB database
+ *         schema:
+ *           type: number
+ *     responses:
+ *       '200':
+ *         description: Employee's all tasks
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
  */
 router.get("/:empId/tasks", async (req, res) => {
   try {
     Employee.findOne(
+      /**
+       * find one employee by id with three fields: empId, todo, and done
+       * return an error message if something wrong with mongo server
+       * otherwise return this user's all tasks
+       */
       { empId: req.params.empId },
       "empId todo done",
       function (err, emp) {
@@ -87,6 +114,7 @@ router.get("/:empId/tasks", async (req, res) => {
       }
     );
   } catch (e) {
+    // internal Server Error
     console.log(e);
     res.status({
       err: config.serverError + ": " + e.message,
@@ -94,61 +122,101 @@ router.get("/:empId/tasks", async (req, res) => {
   }
 });
 
+// Create a tasks by empId, createTask
 /**
- * createTask
+ * @openapi
+ * /api/employees/{empId}/tasks:
+ *   post:
+ *     tags:
+ *       - Employees
+ *     name: createTask
+ *     summary: Creates a new task by empId
+ *     parameters:
+ *        - name: empId
+ *          in: path
+ *          required: true
+ *          description: employee's id is created in MongoDB database
+ *          schema:
+ *            type: number
+ *     requestBody:
+ *       description: Creates a new task API by empId
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             required:
+ *               - text
+ *             properties:
+ *               text:
+ *                 type: string
+ *     responses:
+ *       '200':
+ *         description: New task added to MongoDB
+ *       '401':
+ *         description: Invalid empId
+ *       '500':
+ *         description: Server Exception
+ *       '501':
+ *         description: MongoDB Exception
  */
 router.post("/:empId/tasks", async (req, res) => {
   try {
-    Employee.findOne(
-      { empId: req.params.empId },
-      "empId todo done",
-      function (err, emp) {
-        if (err) {
-          console.log(err);
-          res.status(501).send({
-            err: config.mongoServerError + ": " + err.message,
+    /**
+     * find one employee by id
+     * return an error message if something wrong with mongo server
+     * otherwise return one employee
+     */
+    Employee.findOne({ empId: req.params.empId }, function (err, emp) {
+      if (err) {
+        console.log(err);
+        res.status(501).send({
+          err: config.mongoServerError + ": " + err.message,
+        });
+      } else {
+        console.log(emp);
+
+        /**
+         * If the response is not null (a.k.a. emp)
+         * Add the newTask
+         */
+
+        if (emp) {
+          // if employee creates a new task
+          const newTask = {
+            text: req.body.text,
+          };
+
+          // push the new task to the employee's todo list
+          emp.todo.push(newTask);
+
+          // updates employee with the saved new task
+          emp.save(function (err, updatedEmp) {
+            if (err) {
+              console.log(err);
+              res.status(501).send({
+                err: config.mongoServerError + ": " + err.message,
+              });
+            } else {
+              console.log(updatedEmp);
+              res.json(updatedEmp);
+            }
           });
         } else {
-          console.log(emp);
-
           /**
-           * If the response is not null (a.k.a. emp)
-           * Add the newTask
+           * If the response (a.k.a emp is null)
+           * send 401 error message
            */
-
-          if (emp) {
-            const newTask = {
-              text: req.body.text,
-            };
-
-            emp.todo.push(newTask);
-
-            emp.save(function (err, updatedEmp) {
-              if (err) {
-                console.log(err);
-                res.status(501).send({
-                  err: config.mongoServerError + ": " + err.message,
-                });
-              } else {
-                console.log(updatedEmp);
-                res.json(updatedEmp);
-              }
-            });
-          } else {
-            /**
-             * If the response (a.k.a emp is null)
-             */
-            res.status(401).send({
-              err:
-                "EmployeeId: " +
-                req.params.empId +
-                " does not belong to registered user.",
-            });
-          }
+          res.status(401).send({
+            err:
+              "EmployeeId: " +
+              req.params.empId +
+              " does not belong to registered user.",
+          });
         }
       }
-    );
+    });
   } catch (e) {
+    // internal Server Error
     console.log(e);
     res.status({
       err: config.serverError + ": " + e.message,
